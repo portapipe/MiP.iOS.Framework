8fe777c17a723f37a02cedda338168748ffed83b
portapipe <portapipe@gmail.com>
2013-07-18 16:04:35 +0200

    Create MiPFramework.m
    Creation of basic file

---
+++ MiPFramework.m
@@ -0,0 +1,31 @@
+//
+//  MiPFramework.m
+//  MiP iOS Framework
+//
+//  Created by Norman Russo on 17/07/13.
+//  Copyright (c) since 2013 Norman Russo. All rights reserved.
+//
+
+#import "MiPFramework.h"
+
+@interface MiPFramework ()
+
+@end
+
+@implementation MiPFramework
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
