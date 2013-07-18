7bef6010a998fe59dc3b0f603b087f09912438c4
portapipe <portapipe@gmail.com>
2013-07-18 15:58:27 +0200

    Create MiPFramework.h
    Creation of File

---
+++ MiPFramework.h
@@ -0,0 +1,19 @@
+//
+//  MiPFramework.h
+//  MiP iOS Framework
+//
+//  Created by Norman Russo on 17/07/13.
+//  Copyright (c) since 2013 Norman Russo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController{
+
+}
+
+
+
+
+
+@end
